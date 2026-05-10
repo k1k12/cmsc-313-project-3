a942cce4fa71ea68eb39d95c0e93703084583a46
kiki <sofia@k1k12.localdomain>
2026-05-10 13:56:59 -0400

    feat: open and close file

---
+++ src/hexdump.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  
+  // make sure file runs with one file as arg
+  if (argc != 2) {
+    fprintf(stderr, "Usage: %s <binary file>\n", argv[0]);
+    return 1'
+  }
+
+  // open file in rb
+  FILE *file = fopen(argv[1],"rb");
+
+  // check that file can be opened
+  if (file == NULL) {
+    perror("Error opening file");
+    return 1;
+  }
+
+  // close file
+  fclose(file);
+
+  return 0;
+  
+}
