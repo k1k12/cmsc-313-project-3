098247ca1d89cdd6381e461016d36e357be3e35b
kiki <sofia@k1k12.localdomain>
2026-05-10 14:07:21 -0400

    feat: print file offsets and bytes in hex

--- src/hexdump.c
+++ src/hexdump.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define BYTES_PER_LINE 16
+
+void print_hex_dump(FILE *file) {
+  unsigned char buffer[BYTES_PER_LINE];
+  size_t bytes_read;
+  unsigned int offset = 0;
+
+  while ((bytes_read = fread(buffer, 1, BYTES_PER_LINE, file)) > 0) {
+    // print current byte offset in hex
+    printf("%08x ",offset);
+
+    // print each byte from buffer as 2 hex digits
+    for (size_t i = 0; i < bytes_read; i++) {
+      printf("%02x ", buffer[i]);
+    }
+
+    printf("\n");
+
+    // move offset forward by bytes read
+    offset += bytes_read;
+  }
+}
 
 int main(int argc, char *argv[]) {
   
@@ -17,6 +39,9 @@ int main(int argc, char *argv[]) {
     perror("Error opening file");
     return 1;
   }
+
+  // print hex dump
+  print_hex_dump(file);
 
   // close file
   fclose(file);
